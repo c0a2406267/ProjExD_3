08344cc7b413201a535c53b9b9b872b910fc902b
c0a2406267 <c0a2406267@edu.teu.ac.jp>
2025-04-29 17:30:26 +0900

    追加機能２：完成

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -157,8 +157,9 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
-    beam=None
+    # beam=None
     score=Score()
+    beams=[]  #ビーム空リスト
     # bomb = Bomb((255, 0, 0), 10)
     bombs=[]
     bombs=[Bomb((255,0,0),10) for _ in range(NUM_OF_BOMBS)]
@@ -169,8 +170,8 @@ def main():
             if event.type == pg.QUIT:
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
-                # スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)            
+                # スペースキー押下でBeamクラスのインスタンス生成 
+                beams.append(Beam(bird))          
         screen.blit(bg_img, [0, 0])
 
         # if bomb is not None:
@@ -184,24 +185,31 @@ def main():
                 pg.display.update()
                 time.sleep(1)
                 return
-            
-        
+
+
 
         # if bomb is not None:
         for j,bomb in enumerate(bombs):
-            if beam is not None:
+            for k,beam in enumerate(beams):
+                if beam is None:
+                    continue
                 if beam.rct.colliderect(bomb.rct):  #ビームと爆弾の衝突判定
-                    beam = None #ビームを消す
+                    beams[k] = None #ビームを消す
                     bombs[j] = None  #爆弾を消す
                     bird.change_img(6,screen)  #よろこびエフェクト
                     score.score+=1
             bombs=[bomb for bomb in bombs if bomb is not None] # 撃ち落されていない爆弾
         key_lst = pg.key.get_pressed()
+
+                
+
         bird.update(key_lst, screen)
-        if  beam is not None:
-             beam.update(screen)
+        for beam in beams:
+            if beam is not None:
+                beam.update(screen)
+
         for bomb in bombs:   
-            bomb.update(screen)
+            bomb.update(screen)  
         score.update(screen)   
         pg.display.update()
         tmr += 1
